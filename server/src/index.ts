f609568f0a46dd05053d422d109968e0fa5d9a0c
Rohan0305 <rohanvallapalli@gmail.com>
2026-03-21 00:22:28 -0400

    Logging in index

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,6 +32,8 @@ app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
+console.log("HERE");
+
 // Server
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
